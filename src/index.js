92843a502a6b6bee829da09bd4ca1cb81799459a
Ivanka <IvNagornyuk@gmail.com>
2025-05-03 00:17:09 +0300

    add js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+for (let number = 1; number < 100; number++) {
+  const element = number;
+  if (
+    element === 2 ||
+    element === 3 ||
+    element === 5 ||
+    element === 7 ||
+    (element % 2 !== 0 &&
+      element % 3 !== 0 &&
+      element % 5 !== 0 &&
+      element % 7 !== 0)
+  ) {
+    console.log(element);
+  }
+}
