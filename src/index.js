be897e10a582bf2409c1f280eebc9ce09f29cae6
Ivanka <IvNagornyuk@gmail.com>
2025-05-03 22:45:13 +0300

    add labels

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-for (let number = 2; number < 100; number++) {
-  const element = number;
-    if (
-    element === 2 ||
-    element === 3 ||
-    element === 5 ||
-    element === 7 ||
-    (element % 2 !== 0 &&
-      element % 3 !== 0 &&
-      element % 5 !== 0 &&
-      element % 7 !== 0)
-  ) {
-    console.log(element);
+outer: for (let number = 2; number < 100; number++) {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) continue outer;
   }
+  console.log(number);
 }
